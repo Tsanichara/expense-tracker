d0cca848abe89b9ca50131b085df5606ef5a7b14
Tyler Sanichara <Tsanichara67@gmail.com>
2022-12-22 16:29:47 -0500

    Added section for form input to get expense info

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,23 @@
+import React, {useState} from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
+import Card from '../UI/Card';
 
 function ExpenseItem(props) {
+
+  const [] = useState();
     
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date = {props.date}></ExpenseDate>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>{props.amount}</div>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-    </div>
+      <button>Change Title</button>
+    </Card>
+
   );
 }
 
